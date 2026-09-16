4f987dce4e1b2b3e718c208f540f4f04ff7e9012
Karina's A56 <137480456+K758-hoho@users.noreply.github.com>
2026-09-16 12:22:29 +0700

    News Update

--- detik-rss.py
+++ detik-rss.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
-from feedgen import feed
 from feedgen.feed import FeedGenerator
 import requests
 import sys
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/153.0.0.0 Safari/537.36"
+}
 
 # Define URL
 base_url = "https://news.detik.com/indeks"
@@ -20,7 +23,7 @@ fg.logo(
 )
 
 # Run process if URL is reachable
-response = requests.get(base_url)
+response = requests.get(base_url, headers=headers)
 if response.status_code == 200:
 
     # Get the content of the news
@@ -30,13 +33,12 @@ if response.status_code == 200:
     # Extract the article links
     articles = detik.select("a.media__link")
 
+    # Add every new RSS feed entry
     for a in articles:
         article_link = a["href"]
         article_title = a.get_text(strip=True)
-
         if not article_title:
             continue
-
         fe = fg.add_entry()
         fe.id(article_link)
         fe.title(article_title)
